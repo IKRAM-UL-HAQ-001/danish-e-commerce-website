016a1f1151861cc510eecedb4539f62ea7d10474
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2026-05-15 01:13:14 +0500

    updated contact us

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -92,7 +92,7 @@
 
         <div class="map fix">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52872517.59607392!2d-161.691169406869!3d36.018281840171966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1769883541208!5m2!1sen!2sbd"></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52872517.59607392!2d-161.691169406869!3d36.018281840171966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20Kingdom!5e0!3m2!1sen!2sbd!4v1769883541208!5m2!1sen!2sbd"></iframe>
         </div>
 
       
